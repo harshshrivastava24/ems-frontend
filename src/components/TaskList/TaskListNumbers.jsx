1827de41aeb38114a66c231c8022fbaf2a8f2799
Harsh Shrivastava <harshshrivastava2403@gmail.com>
2025-07-16 22:55:14 +0530

    using context

--- src/components/TaskList/TaskListNumbers.jsx
+++ src/components/TaskList/TaskListNumbers.jsx
@@ -8,15 +8,15 @@ const TaskListNumbers = ({ data }) => {
                 <h3 className='text-lg font-semibold'>New Task</h3>
             </div>
             <div className='p-10 bg-green-400 w-[45%] rounded-lg'>
-                <h1 className='text-3xl font-bold'>3</h1>
+                <h1 className='text-3xl font-bold'>{data.taskCount.completed}</h1>
                 <h3 className='text-lg font-semibold'>Completed</h3>
             </div>
             <div className='p-10 bg-yellow-400 w-[45%] rounded-lg'>
-                <h1 className='text-3xl font-bold'>0</h1>
+                <h1 className='text-3xl font-bold'>{data.taskCount.active}</h1>
                 <h3 className='text-lg font-semibold'>Accepted</h3>
             </div>
             <div className='p-10 bg-red-400 w-[45%] rounded-lg'>
-                <h1 className='text-3xl font-bold'>0</h1>
+                <h1 className='text-3xl font-bold'>{data.taskCount.failed}</h1>
                 <h3 className='text-lg font-semibold'>Failed</h3>
             </div>
         </div>
