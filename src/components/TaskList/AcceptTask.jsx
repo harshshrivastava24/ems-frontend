c67bc25be35fe7ba525b7574460258e88405a85f
Harsh Shrivastava <harshshrivastava2403@gmail.com>
2025-07-26 23:33:19 +0530

    Dynamic rendering

--- src/components/TaskList/AcceptTask.jsx
+++ src/components/TaskList/AcceptTask.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 
-const AcceptTask = () => {
+const AcceptTask = ({ data }) => {
     return (
         <div className='shrink-0 h-[300px] w-[300px] bg-blue-400 rounded-lg text-white p-5'>
             <div className='flex justify-between items-center'>
-                <h1 className='bg-red-500 px-3 py-1 rounded text-sm'>High</h1>
-                <h4 className='text-sm'>27 May 2025</h4>
+                <h1 className='bg-red-500 px-3 py-1 rounded text-sm'>{data.category}</h1>
+                <h4 className='text-sm'>{data.taskDate}</h4>
             </div>
-            <h2 className='mt-5 text-2xl font-semibold'>Make a Youtube video</h2>
-            <p className='mt-2 text-sm'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Doloribus, maiores!</p>
+            <h2 className='mt-5 text-2xl font-semibold'>{data.title}</h2>
+            <p className='mt-2 text-sm'>{data.description}</p>
             <div className='mt-10 flex justify-evenly items-center'>
                 <button className='px-3 py-3 text-sm rounded-md items-center bg-green-500 w-1/3'>Mark as Completed</button>
                 <button className='px-3 py-3 text-sm rounded-md items-center bg-red-500 w-1/3'>Mark as Failed</button>
--- src/components/others/Header.jsx
+++ src/components/others/Header.jsx
@@ -2,11 +2,16 @@ import React from 'react'
 
 const Header = ({ data }) => {
     // console.log(data)
+    const logOutUser = () => {
+        localStorage.setItem('isLoggedIn', '')
+        window.location.reload()
+    }
+
     return (
         <>
             <div className='flex items-end justify-between text-white'>
                 <h1 className='text-xl font-semibold'>Hello <br /> <span className='text-2xl font-bold'>{data.firstName} 👋!</span></h1>
-                <button className='border-none rounded-lg bg-red-600 p-2'>Logout</button>
+                <button className='border-none rounded-lg bg-red-600 p-2' onClick={logOutUser}>Logout</button>
             </div>
             <div></div>
         </>
